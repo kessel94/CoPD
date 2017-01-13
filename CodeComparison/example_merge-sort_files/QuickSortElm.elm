5241635cd385dca1dd6edc889b3e3fd8b82f5dc6
Enzo <kessel94@live.nl>
2017-01-13 20:00:28 +0100

    Types
    Subject by Paolo

--- CodeComparison/example_merge-sort_files/QuickSortElm.elm
+++ CodeComparison/example_merge-sort_files/QuickSortElm.elm
@@ -1,6 +1,19 @@
-main = asText (qsort [3,9,1,8,5,4,7])
+import Html exposing (text)
 
-qsort lst =
-  case lst of
-    x:xs -> qsort (filter ((>=)x) xs) ++ [x] ++ qsort (filter ((<)x) xs)
-    [] -> []
+
+main =
+  text (toString (quicksort [5,3,8,1,9,4,7]))
+
+
+quicksort : List comparable -> List comparable
+quicksort list =
+  case list of
+    [] ->
+        []
+
+    pivot :: rest ->
+        let
+          lower  = List.filter (\n -> n <= pivot) rest
+          higher = List.filter (\n -> n >  pivot) rest
+        in
+          quicksort lower ++ [pivot] ++ quicksort higher
---
+++ CodeComparison/example_merge-sort_files/code.elm
@@ -0,0 +1,70 @@
+
+
+main =
+  text (toString (mergesort [5,3,8,1,9,4,7]))
+
+
+{-| Sorts a list of values by:
+
+  1. If the list has zero or one element, it is sorted!
+  2. If it is longer, split it into two sublists
+  3. Sort those sublists
+  4. Merge these sorted sublists together
+
+-}
+mergesort : List comparable -&gt; List comparable
+mergesort list =
+  case list of
+    [] -&gt;
+        list
+
+    [_] -&gt;
+        list
+
+    _ -&gt;
+        let
+          (xs, ys) = split list
+        in
+          merge (mergesort xs) (mergesort ys)
+
+
+{-| Split a list into two sublists of nearly equal length.
+
+    split [1,2,3,4,5,6] == ([2,4,6], [1,3,5])
+
+-}
+split : List a -&gt; (List a, List a)
+split list =
+  case list of
+    [] -&gt;
+        ([], [])
+
+    x :: rest -&gt;
+        let
+          (xs, ys) = split rest
+        in
+          (ys, x :: xs)
+
+
+{-| Given two sorted lists, merge them together into a single
+sorted list.
+
+    merge [1,4,9] [2,3,5] == [1,2,3,4,5,9]
+
+-}
+merge : List comparable -&gt; List comparable -&gt; List comparable
+merge xs ys =
+  case (xs, ys) of
+    (x :: xBack, y :: yBack) -&gt;
+        if x &lt; y then
+          x :: merge xBack ys
+        else
+          y :: merge xs yBack
+
+    ([], rest) -&gt;
+        rest
+
+    (rest, []) -&gt;
+        rest
+
+<
